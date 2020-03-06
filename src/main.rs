74ce7516351e9bd757321e38b262883aae8cd615
Scott Lee <scott.lee@nucleusscientific.com>
2020-03-06 15:32:58 -0500

    rectangle

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,28 @@
-use std::io;
-use rand::Rng;
-use std::cmp::Ordering;
+#[derive(Debug)]
+struct  Rectangle {
+    width: u32,
+    height: u32,
+}
+
+impl Rectangle {
+
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+
+}
 
 fn main() {
-    let x = 5;
-    println!("The value of x is: {}", x);
-    let x = 6;
-    println!("The value of x is: {}", x);
+
+    let rect1 = Rectangle {width: 30, height: 50};
+
+    println!(
+        "The area of the rectangle is {} square pixels.",
+        rect1.area()
+    );
+
 }
